83fa419f9552d6caffdffc661a6dd33f3ef66372
Marcelle Meintjes <mlouise.meintjes@gmail.com>
2024-07-30 18:45:35 +0200

    first commit

---
+++ chat.js
@@ -0,0 +1,9 @@
+// Variables for real-time chat application : Mingle Box
+var messages = document.getElementById("messages");
+var textbox = document.getElementById("textbox");
+var button = document.getElementById("button");
+var usernameInput = document.getElementById("username");
+var joinButton = document.getElementById("join-btn");
+var chatArea = document.getElementById("chat-area");
+var onlineUserList = document.getElementById("user-list");
+
